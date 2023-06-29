ed046615c3b52cd08df166e7c3818a0d61e76a96
Guillermo Ramirez <erguile@hotmail.com>
2023-06-29 11:32:39 +0100

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoPacientes from './components/ListadoPacientes';
+
+
+
+function App() {
+
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem("pacientes")) ?? [])
+  const [paciente, setPaciente] = useState({})
+
+
+
+  useEffect(()=>{
+    localStorage.setItem("pacientes", JSON.stringify(pacientes))
+  },[pacientes])
+
+
+  // useEffect(() => {
+  //   const obtenerLS = ()=>{
+  //     const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+  //     setPacientes(pacientesLS)
+  //   }
+  //   obtenerLS()
+  // }, [])
+
+  // useEffect(() => {
+  //   localStorage.setItem("pacientes", JSON.stringify(pacientes))
+  // }, [pacientes])
+
+  const eliminarPaciente = (id) =>{
+    // console.log("Eliminando paciente", id)
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+  
+
+  return (
+    <>
+      <Header 
+        
+      />
+      <div className='mt-12 md:flex'>
+        <Formulario 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          paciente={paciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+       
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header({}) {
+
+
+
+
+    return (
+        <>
+    
+            <div className="font-black text-5xl 
+            text-center w-2/3 md:mx-auto">
+                <h1>{}</h1>
+                <h1>Seguimientos Pacientes{""} <span className="text-indigo-600">Veterinaria</span></h1>
+            </div>
+        </>
+    )
+}
